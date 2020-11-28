eebffde7afd12e8cdf50bf189b14a345d8749f4c
SangJin Jung <redcaprabbit1104@gmail.com>
2020-11-28 15:55:50 +0900

    sencond commit
    user.rbのdeviseで:validatableを削除した際に、validatesで:emailを指定し忘れてました。修正しましたのどご確認ください。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
          #⏫上記から :validatableを削除、これによりpasswordをカスタマイズできる
-  validates :name, :profile, :occupation, :position, presence: true
+  validates :name, :profile, :occupation, :position, :email, presence: true
   validates :password, presence: true, length: { minimum: 6 } #パスワードを6文字以上に制限
   has_many :prototypes
   has_many :comments
